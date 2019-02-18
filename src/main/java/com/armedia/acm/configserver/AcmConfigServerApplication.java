fbf6a84b2b01688e24536397c58ee5065122e61d
Nadica Cuculova <nadica.cuculova@armedia.com>
2019-02-18 12:18:54 +0100

    AFDP-6968: Monitor config server repository for file changes and send jms message to "configuration.changed" topic on updates;

--- src/main/java/com/armedia/acm/configserver/AcmConfigServerApplication.java
+++ src/main/java/com/armedia/acm/configserver/AcmConfigServerApplication.java
@@ -1,18 +1,27 @@
 package com.armedia.acm.configserver;
 
+import com.armedia.acm.configserver.service.FileWatchService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableConfigServer
 @SpringBootApplication
 @ConfigurationProperties
+@EnableJms
+@EnableAsync
 public class AcmConfigServerApplication
 {
 
     public static void main(String[] args)
     {
-        SpringApplication.run(AcmConfigServerApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(AcmConfigServerApplication.class, args);
+
+        FileWatchService fileWatchService = context.getBean(FileWatchService.class);
+        fileWatchService.monitor();
     }
 }
