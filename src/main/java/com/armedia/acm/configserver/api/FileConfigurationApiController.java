3eba21f44fde03e776ce92223cfb521c7cdcfd87
Mario Gjurcheski <mario.gjurcheski@armedia.com>
2019-09-02 09:48:00 -0400

    AFDP-7891 - add temporary log messages for debug purposes.

--- src/main/java/com/armedia/acm/configserver/api/FileConfigurationApiController.java
+++ src/main/java/com/armedia/acm/configserver/api/FileConfigurationApiController.java
@@ -1,6 +1,8 @@
 package com.armedia.acm.configserver.api;
 
 import com.armedia.acm.configserver.service.FileConfigurationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,12 +20,17 @@ public class FileConfigurationApiController {
     @Autowired
     FileConfigurationService fileConfigurationService;
 
+    private static final Logger logger = LoggerFactory.getLogger(FileConfigurationApiController.class);
+
     @PostMapping()
     public ResponseEntity moveFileToConfiguration(@RequestParam("file") MultipartFile file,
                                                   @RequestParam("fileName") String fileName) throws Exception {
 
+        logger.info("branding file is received on config server! [{}]", fileName);
 
         fileConfigurationService.moveFileToConfiguration(file, fileName);
+
+        logger.info("file is moved to config server!");
 
         return ResponseEntity.ok().build();
     }
