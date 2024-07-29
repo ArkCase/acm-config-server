5403c312e2c2e421cf2723491a420263587eadda
Vladimir Cherepnalkovski <v.cherepnalkovski@armedia.com>
2024-07-29 13:37:30 +0200

    Move import logic in one service

--- src/main/java/com/armedia/acm/configserver/service/DataImporterService.java
+++ src/main/java/com/armedia/acm/configserver/service/DataImporterService.java
@@ -27,30 +27,50 @@ package com.armedia.acm.configserver.service;
  * #L%
  */
 
+import static com.armedia.acm.configserver.service.ConfigUtils.extractFromFileName;
+import static com.armedia.acm.configserver.service.ConfigUtils.findProfile;
+
+import com.armedia.acm.configserver.model.ApplicationProperty;
 import com.armedia.acm.configserver.model.ArkcaseConfig;
 import com.armedia.acm.configserver.repository.ApplicationPropertyRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Profile("run-with-db")
-public class DataImporterService extends DataService
+public class DataImporterService
 {
+    private static final Logger logger = LoggerFactory.getLogger(DataImporterService.class);
+    protected final ApplicationPropertyRepository applicationPropertyRepository;
+    protected final ArkcaseConfig arkcaseConfig;
+    protected final ObjectMapper mapper;
 
-    protected DataImporterService(ApplicationPropertyRepository applicationPropertyRepository, ArkcaseConfig arkcaseConfig)
+    public DataImporterService(ApplicationPropertyRepository applicationPropertyRepository, ArkcaseConfig arkcaseConfig)
     {
-        super(applicationPropertyRepository, arkcaseConfig);
+        this.applicationPropertyRepository = applicationPropertyRepository;
+        this.arkcaseConfig = arkcaseConfig;
+        this.mapper = new ObjectMapper(new YAMLFactory());
     }
 
     public void importData()
     {
-        if (Boolean.TRUE.equals(this.applicationPropertyRepository.existsAnyRecord()))
-            return;
-
         var resolver = new PathMatchingResourcePatternResolver();
 
         for (var folder : this.arkcaseConfig.getFolders())
@@ -60,13 +80,141 @@ public class DataImporterService extends DataService
                 var resources = resolver.getResources("file:" + folder + "/*.yaml");
                 for (var resource : resources)
                 {
-                    super.readResource(resource);
+                    readResource(resource);
+
+                    deleteResource(resource);
                 }
             }
             catch (IOException e)
             {
-                log.error("Unable to resolve resources in folder: {}, reason: {}", folder, e.getMessage(), e);
+                DataImporterService.logger.error("Unable to resolve resources in folder: {}, reason: {}", folder, e.getMessage(), e);
             }
+        }
+    }
+
+    private void readResource(Resource resource)
+    {
+        DataImporterService.logger.debug("Import runtime properties from file: {}", resource.getFilename());
+        try (var reader = new BufferedReader(
+                new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)))
+        {
+            readFromYaml(resource.getFilename(), reader);
+        }
+        catch (IOException e)
+        {
+            DataImporterService.logger.debug("Unable to read resource: {}, reason:{}", resource.getFilename(), e.getMessage(), e);
+        }
+    }
+
+    private void readFromYaml(String fileName, BufferedReader reader) throws IOException
+    {
+        if (reader.ready())
+        {
+            try
+            {
+                var yamlProperties = mapper.readValue(reader, Map.class);
+                if (yamlProperties != null && !yamlProperties.isEmpty())
+                {
+                    var applicationProperties = yamlToApplicationProperties(fileName, yamlProperties);
+                    if (fileName.contains("-runtime"))
+                    {
+                        saveRuntimeProperties(applicationProperties);
+                    }
+                    else
+                    {
+                        this.applicationPropertyRepository.saveAll(applicationProperties);
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                DataImporterService.logger.debug("Unable to read YAML file: {}, reason:{}", fileName, e.getMessage(), e);
+            }
+        }
+        else
+        {
+            DataImporterService.logger.warn("YAML file is empty: {}", fileName);
+        }
+    }
+
+    private void saveRuntimeProperties(List<ApplicationProperty> applicationProperties)
+    {
+        applicationProperties.forEach(ap -> {
+            var existingAp = this.applicationPropertyRepository.findByApplicationAndProfileAndLabelAndKey(
+                    ap.getApplication(),
+                    ap.getProfile(), ap.getLabel(), ap.getKey());
+            if (existingAp.isEmpty())
+            {
+                this.applicationPropertyRepository.save(ap);
+            }
+            else
+            {
+                existingAp.get().setValue(ap.getValue());
+                this.applicationPropertyRepository.save(existingAp.get());
+            }
+        });
+    }
+
+    private List<ApplicationProperty> yamlToApplicationProperties(String fileName, Map<String, Object> yamlProperties)
+    {
+        var applicationProperties = new ArrayList<ApplicationProperty>();
+
+        if (fileName == null || fileName.isBlank())
+            return List.of();
+
+        var label = extractFromFileName(fileName, this.arkcaseConfig.getLanguages());
+
+        var matchedProfile = findProfile(fileName, arkcaseConfig.getProfiles());
+        var profile = matchedProfile.orElse("default");
+
+        var applicationName = matchedProfile.map(s -> fileName.substring(0, fileName.indexOf(s) - 1))
+                .orElseGet(() -> fileName.substring(0, fileName.lastIndexOf('.')));
+
+        var flattenedProperties = new HashMap<String, String>();
+        flattenProperties("", yamlProperties, flattenedProperties);
+
+        for (var entry : flattenedProperties.entrySet())
+        {
+            var applicationProperty = new ApplicationProperty(applicationName, profile, label, entry.getKey(), entry.getValue());
+            applicationProperties.add(applicationProperty);
+        }
+
+        return applicationProperties;
+    }
+
+    private void flattenProperties(String prefix, Map<String, Object> source, Map<String, String> target)
+    {
+        try
+        {
+            source.forEach((key, value) -> {
+                var newKey = prefix.isEmpty() ? key : prefix + "." + key;
+                if (value instanceof Map)
+                {
+                    flattenProperties(newKey, (Map<String, Object>) value, target);
+                }
+                else
+                {
+                    target.put(newKey, String.valueOf(value));
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            DataImporterService.logger.error("Unable to flatten prefix:{}, source:{}, reason: {}", prefix, source, e.getMessage(), e);
+        }
+    }
+
+    private void deleteResource(Resource resource)
+    {
+        try
+        {
+            var path = resource.getFile().toPath();
+            Files.delete(path);
+        }
+        catch (IOException e)
+        {
+            DataImporterService.logger.warn("Failed to delete file: {}, reason: {}", resource.getFilename(), e.getMessage());
         }
     }
 }
