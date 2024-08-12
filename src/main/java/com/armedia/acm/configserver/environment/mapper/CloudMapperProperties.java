a27540bde13db2a4c25a09fb3f686badebc2c831
diego <diego.rivera@armedia.com>
2024-08-12 15:54:06 -0600

    Compute the pod's default namespace, if possible

--- src/main/java/com/armedia/acm/configserver/environment/mapper/CloudMapperProperties.java
+++ src/main/java/com/armedia/acm/configserver/environment/mapper/CloudMapperProperties.java
@@ -26,18 +26,51 @@
  */
 package com.armedia.acm.configserver.environment.mapper;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+
+import io.kubernetes.client.util.Config;
 
 @Lazy
 @Component
 public class CloudMapperProperties
 {
+    private static final Logger LOG = LoggerFactory.getLogger(CloudMapperProperties.class);
+
+    private static final Path SERVICEACCOUNT_NAMESPACE_PATH = Paths.get(Config.SERVICEACCOUNT_NAMESPACE_PATH);
+
     protected static final Boolean DEFAULT_ENABLED = Boolean.TRUE;
+    protected static final String DEFAULT_NAMESPACE;
+    static
+    {
+        // If the namespace is not given, grab the running pods' namespace
+        String namespace = null;
+        try (Reader r = Files.newBufferedReader(CloudMapperProperties.SERVICEACCOUNT_NAMESPACE_PATH, StandardCharsets.UTF_8))
+        {
+            namespace = IOUtils.toString(r);
+        }
+        catch (IOException e)
+        {
+            CloudMapperProperties.LOG.warn("Failed to read the pod's namespace from the file at [%s], it must be configured explicitly",
+                    CloudMapperProperties.SERVICEACCOUNT_NAMESPACE_PATH,
+                    e);
+        }
+        DEFAULT_NAMESPACE = namespace;
+    }
+
     protected static final Boolean DEFAULT_DISABLE_INTERPOLATOR = Boolean.FALSE;
     protected static final Boolean DEFAULT_MISSING_AS_EMPTY = Boolean.FALSE;
 
@@ -55,7 +88,7 @@ public class CloudMapperProperties
             @Value("${cloud-mapper.disableInterpolator:#{null}}") Boolean disableInterpolator,
             @Value("${cloud-mapper.missingAsEmpty:#{null}}") Boolean missingAsEmpty)
     {
-        this.namespace = namespace;
+        this.namespace = Objects.requireNonNullElse(namespace, CloudMapperProperties.DEFAULT_NAMESPACE);
         this.enabled = StringUtils.isNotBlank(this.namespace) && Objects.requireNonNullElse(enabled, CloudMapperProperties.DEFAULT_ENABLED);
         this.disableInterpolator = Objects.requireNonNullElse(disableInterpolator, CloudMapperProperties.DEFAULT_DISABLE_INTERPOLATOR);
         this.missingAsEmpty = Objects.requireNonNullElse(missingAsEmpty, CloudMapperProperties.DEFAULT_MISSING_AS_EMPTY);
